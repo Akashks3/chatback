78c6bb318fb33d36d781ffc2434bbff4bf92f36f
akash <155128336+Akashks3@users.noreply.github.com>
2025-03-13 11:42:07 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -6,10 +6,6 @@ const userSchema = new mongoose.Schema(
 			type: String,
 			required: true,
 		},
-		lastName: {
-			type: String,
-			required: true,
-		},
 		password: {
 			type: String,
 			required: true,
